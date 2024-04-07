6bcbdc006f638596654fdb3e1f04beca8f94d634
bruce pedro gomes <brucegomes1337@gmail.com>
2024-04-07 12:48:10 -0300

    feat: chakra-ui setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import "@/styles/globals.css";
+
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { fonts } from "@/libs/fonts";
+import { theme } from "@/theme";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-rubik: ${fonts.rubik.style.fontFamily};
+          }
+        `}
+      </style>
+      <Head>
+        <title>Team Balance</title>
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { ThemeConfig, extendTheme } from "@chakra-ui/react";
+
+
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+export const theme = extendTheme({
+  config,
+  fonts: {
+    heading: "var(--font-rubik)",
+    body: "var(--font-rubik)",
+  },
+});
